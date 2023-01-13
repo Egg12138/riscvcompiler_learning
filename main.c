573091a5434eb13925cf42d1261ea4773175d2a5
Egg12138 <zhejunxiao@126.com>
2023-01-13 11:37:40 +0800

    finish /*() parsing

--- main.c
+++ main.c
@@ -4,12 +4,35 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
+
+#define NUM_BASE 10
+/* 这里我们统一用load double : ld rd, offset(rs1) 所以是8 bytes = 64bits*/
+#define SP_PUSH_SHIFT -8
+#define SP_POP_SHIFT  8
 
 typedef enum {
-    TK_OPERATOR,
-    TK_NUM,
+    TK_PUNCT,
+    TK_DIGIT,
     TK_EOF,
 } TokenKind;
+
+typedef enum {
+    ND_ADD,
+    ND_SUB,
+    ND_MUL,
+    ND_DIV,
+    ND_DIGIT,
+} NodeKind;
+
+typedef struct Node ASTNode;
+struct Node {
+    NodeKind kind;
+    ASTNode *lhs;
+    ASTNode *rhs;
+    int val;
+};
+typedef ASTNode* AST;
 
 typedef struct Token Token;
 struct Token {
@@ -70,6 +93,7 @@ static Token *tokenize(){
     Token head = {};
     Token *cur = &head;
     char *p = input_string;
+
     while (*p) {
         if (isspace(*p)) {
             /* skip */
@@ -78,27 +102,24 @@ static Token *tokenize(){
         }
 
         if (isdigit(*p)) {
-            /* parse digit */
-            // TODO:`nextToken(kind: TokenKind, ptr: char *)`
-            cur->next = newToken(TK_NUM, p, p);
+            cur->next = newToken(TK_DIGIT, p, p);
             cur = cur->next;
+            const char *prev_ptr = p;
             // NOTICE: strtol函数才是捕捉str的关键，*p++这样只是得到每一个char. 
             // strtol是把传入指针从头开始的连续数字进行解析，如果开头就不是数字那就不解析了
             // strtol这里将p进行了移动。
-            const char *prev_ptr = p;
-            cur->val = strtol(p, &p, 10);//base = 10 
-           // NOTICE: loc = 0？
+            cur->val = strtol(p, &p, NUM_BASE);//base = 10 
             cur->len = p - prev_ptr; 
             continue;
         }
-        if (*p == '+' || *p == '-') {
-            cur->next = newToken(TK_OPERATOR, p, p+1);
+        if (ispunct(*p)){
+            cur->next = newToken(TK_PUNCT, p, p+1);
             cur = cur->next;
             p++;
-           continue; 
+            continue; 
         }
 
-        error_at(p, "invalid token: %c");
+        error_at(p, "invalid token: %c", *p);
     } 
     // add an EOF
     cur->next = newToken(TK_EOF, p, p);
@@ -106,7 +127,7 @@ static Token *tokenize(){
 }
 
 int numberFrom(Token *token) {
-   if (token->kind != TK_NUM) {
+   if (token->kind != TK_DIGIT) {
       error("Got [%d], expect a number", token->val); 
    }
    return token->val;
@@ -123,36 +144,171 @@ static Token *skip(Token *token, char *str) {
   return token->next;
 }
 
+static ASTNode *newNode(NodeKind kind) {
+    ASTNode *node = calloc(1, sizeof(ASTNode));
+    node->kind = kind;
+    return node;
+}
 
+static ASTNode *newBTNode(NodeKind kind, ASTNode *lhs, ASTNode *rhs) {
+    ASTNode *node = newNode(kind); 
+    node->lhs = lhs;
+    node->rhs = rhs;
+    return node;
+}
+
+static ASTNode *newDigitNode(int number) {
+    ASTNode *node = newNode(ND_DIGIT);
+    node->val = number;
+    return node;
+}
+
+static ASTNode *expr(Token **rest, Token *token);
+static ASTNode *mul(Token **rest, Token *token);
+static ASTNode *primary(Token **rest, Token *token);
+
+static ASTNode *expr(Token **rest, Token *token) {
+    ASTNode *node = mul(&token, token);
+    while (true) {
+        if (eq(token, "+")) {
+            node = newBTNode(ND_ADD, node, mul(&token, token->next));
+            continue;
+        }
+
+        if (eq(token, "-")) {
+            node = newBTNode(ND_SUB, node, mul(&token, token->next));
+            continue;
+        }
+        // 是+/-就会分割exprssion
+        *rest = token;
+        return node;
+    }
+}
+
+static ASTNode *mul(Token **rest, Token *token) {
+    ASTNode *node = primary(&token, token);
+    while (true) {
+        if (eq(token, "*")) {
+            node = newBTNode(ND_MUL, node, primary(&token, token->next));
+            continue;
+        }
+        if (eq(token, "/")) {
+            node = newBTNode(ND_DIV, node, primary(&token, token->next));
+            continue;
+        }
+        *rest = token;
+        return node;
+    } 
+}
+
+static ASTNode *primary(Token **rest, Token *token) {
+    ASTNode *node;
+    if (eq(token, "(")) {
+        node = expr(&token, token->next);
+        *rest = skip(token, ")"); // the next token
+        return node;
+    } 
+
+    if (token->kind == TK_DIGIT) {
+        node = newDigitNode(token->val);
+        *rest = token->next;
+        return node;
+    }
+
+    error_at_token(token, "expected an expression");
+    return NULL;
+}
+
+static int depth = 0;
+// sd rs2, offset(rs1): 将x[rs2]中的8bytes->x[rs1]+extend_offset
+static void push_to_stack(void) {
+    printf(" addi sp, sp, %d\n", SP_PUSH_SHIFT);
+    printf(" sd a0, 0(sp)\n");
+    depth++;
+}
+
+static void pop_to(char *target_rs) {
+    printf(" ld %s, 0(sp)\n", target_rs); // 将sp + offset:0处的双字(2*4bytes)写到target_rs中
+    printf(" addi sp, sp, %d\n", SP_POP_SHIFT);                      // 然后再移动栈指针
+    depth--; 
+} 
+
+// TODO: 这种递归在这种小算式中效率没有什么问题。
+// 递归地生成，右枝优先. 最顶层地传入为AST ast根节点. LHS->a1, RHS->a0
+static void gen_expr(ASTNode *node) {
+    if (node->kind == ND_DIGIT) {
+        printf(" li a0, %d\n", node->val);
+        return;
+    } 
+    // if puncts:
+    gen_expr(node->rhs);
+    push_to_stack();
+    gen_expr(node->lhs);
+    pop_to("a1");
+
+    switch (node->kind)
+    {
+    case ND_ADD:
+        printf(" add a0, a0, a1\n");
+        return;
+    case ND_SUB: 
+        printf(" sub a0, a0, a1\n");
+        return;
+    case ND_MUL:
+        printf(" mul a0, a0, a1\n");
+        return;
+    case ND_DIV:
+        printf(" div a0, a0, a1\n");
+        return;
+    default:
+        error("invalid expression!");
+    }
+
+}
+/**
+ * 1. check input => valid arguments
+ * 2. parse arguments => token series  
+ * 3. parse token series & move the tokens' marker to EOF => AST 
+ * 4. parse AST => expression
+ * 5. generate expr => tmp.s
+ * 6. some other things.
+*/
 int
 main(int argc, char **argv)
 {
 
+    //1
     if (argc != 2) {
         error("%s: invalid number of arguments!", argv[0]);
     }
-
     input_string = argv[1];
+    //2
     Token *token = tokenize();
-    // char *p = " 12 + 34 - 27 ";
-    // Token *token = tokenize(" 12 + 34 -27");
+    //3
+    AST ast = expr(&token, token); 
+    if (token->kind != TK_EOF)
+        error_at_token(token, "extra token after EOF");
+
     printf("  .globl main\n");
     printf("main:\n");
-    printf(" li a0, %d\n", numberFrom(token)); 
-    token = token->next;
-    while (token->kind != TK_EOF) {
-        if (eq(token, "+")) {
-            token = token->next;
-            printf(" addi a0, a0, %d\n", numberFrom(token));
-            token = token->next;
-            continue;
-        } 
-        // else => '-'
-        token = skip(token, "-");
-        printf(" addi a0, a0, -%d\n", numberFrom(token));
-        token = token->next;
-    }
+
+    gen_expr(ast);
+    // printf(" li a0, %d\n", numberFrom(token)); 
+    // token = token->next;
+    // while (token->kind != TK_EOF) {
+    //     if (eq(token, "+")) {
+    //         token = token->next;
+    //         printf(" addi a0, a0, %d\n", numberFrom(token));
+    //         token = token->next;
+    //         continue;
+    //     } 
+    //     // else => '-'
+    //     token = skip(token, "-");
+    //     printf(" addi a0, a0, -%d\n", numberFrom(token));
+    //     token = token->next;
+    // }
     printf(" ret\n");
-      return 0;
+    assert(depth==0);
+    return 0;
 }
 
--- test.sh
+++ test.sh
@@ -2,6 +2,7 @@
 assert() {
     expected="$1" # the first argument
     input="$2"
+    id="$3"
     ./rvcc "$input" > tmp.s || exit  # OR exit
     # RISCV64平台编译
     riscv64-linux-gnu-gcc -static -o tmp tmp.s  
@@ -11,10 +12,10 @@ assert() {
     echo "test $input..."
     if [ "$actual" = "$expected" ]; then
         echo "\tleft: \`$input\` == right: \`$actual\`"
-        echo "\t.[Ok]."
+        echo "\ttest:[$id]...Ok."
     else
         echo "\tleft:\`$input\` != right:\`$actual\`, \`$expected\` is expected!"
-        echo "\t.[Failed]."
+        echo "\ttest:[$id]...Failed."
         #exit 1
     fi
 }
@@ -22,15 +23,19 @@ assert() {
 
 # assert 期待值 输入值
 # [1] 返回指定数值
-# assert 0 0
-# assert 42 42
+assert 0 0 1
+assert 42 42 2
 
 # [2] 支持+ -运算符
-# assert 34 '12-34+56'
+assert 34 '12-34+56' 3
 
 # [3] 支持空格
-assert  120  "   70-58+108"
-assert 128 '64+ 64'
-assert 41 ' 12 + 34 - 5 '
+assert  120  "   70-58+108" 4
+assert 128 '64+ 64' 5 
+assert 41 ' 12 + 34 - 5 ' 6
+assert 56 '(3+5)*7' 括号优先
+assert  30 '3 + 5*7-8' 四则顺序
+assert  37  '3 + ((1+2)*(2+3) + 2) *2' 括号嵌套
+assert  -1    '3+((1+2)*(2+3)+2 * 2' 括号不合法
 
 echo "All done"
